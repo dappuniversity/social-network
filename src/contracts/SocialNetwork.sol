b1206ce0459911ee81d96a7bb6de6953250896e4
Gregory McCubbin <gwmccubbin@gmail.com>
2019-11-14 18:09:51 -0600

    2. Create posts

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -2,8 +2,35 @@ pragma solidity ^0.5.0;
 
 contract SocialNetwork {
     string public name;
+    uint public postCount = 0;
+    mapping(uint => Post) public posts;
+
+    struct Post {
+        uint id;
+        string content;
+        uint tipAmount;
+        address payable author;
+    }
+
+    event PostCreated(
+        uint id,
+        string content,
+        uint tipAmount,
+        address payable author
+    );
 
     constructor() public {
         name = "Dapp University Social Network";
+    }
+
+    function createPost(string memory _content) public {
+        // Require valid content
+        require(bytes(_content).length > 0);
+        // Increment the post count
+        postCount ++;
+        // Create the post
+        posts[postCount] = Post(postCount, _content, 0, msg.sender);
+        // Trigger event
+        emit PostCreated(postCount, _content, 0, msg.sender);
     }
 }
