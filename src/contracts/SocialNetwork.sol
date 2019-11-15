5c76573ce3df6a3bed397061101bff5b32c37822
Gregory McCubbin <gwmccubbin@gmail.com>
2019-11-14 18:11:06 -0600

    3. Tip posts

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -19,6 +19,13 @@ contract SocialNetwork {
         address payable author
     );
 
+    event PostTipped(
+        uint id,
+        string content,
+        uint tipAmount,
+        address payable author
+    );
+
     constructor() public {
         name = "Dapp University Social Network";
     }
@@ -32,5 +39,22 @@ contract SocialNetwork {
         posts[postCount] = Post(postCount, _content, 0, msg.sender);
         // Trigger event
         emit PostCreated(postCount, _content, 0, msg.sender);
+    }
+
+    function tipPost(uint _id) public payable {
+        // Make sure the id is valid
+        require(_id > 0 && _id <= postCount);
+        // Fetch the post
+        Post memory _post = posts[_id];
+        // Fetch the author
+        address payable _author = _post.author;
+        // Pay the author by sending them Ether
+        address(_author).transfer(msg.value);
+        // Incremet the tip amount
+        _post.tipAmount = _post.tipAmount + msg.value;
+        // Update the post
+        posts[_id] = _post;
+        // Trigger an event
+        emit PostTipped(postCount, _post.content, _post.tipAmount, _author);
     }
 }
