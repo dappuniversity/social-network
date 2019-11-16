d4d4fa73c23c491957eb19c335b036054047168e
Gregory McCubbin <gwmccubbin@gmail.com>
2019-11-15 18:05:32 -0600

    Adds front end

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,103 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3';
+import Identicon from 'identicon.js';
 import './App.css';
+import SocialNetwork from '../abis/SocialNetwork.json'
+import Navbar from './Navbar'
+import Main from './Main'
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    // Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = SocialNetwork.networks[networkId]
+    if(networkData) {
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
+      this.setState({ socialNetwork })
+      const postCount = await socialNetwork.methods.postCount().call()
+      this.setState({ postCount })
+      // Load Posts
+      for (var i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call()
+        this.setState({
+          posts: [...this.state.posts, post]
+        })
+      }
+      // Sort posts. Show highest tipped posts first
+      this.setState({
+        posts: this.state.posts.sort((a,b) => b.tipAmount - a.tipAmount )
+      })
+      this.setState({ loading: false})
+    } else {
+      window.alert('SocialNetwork contract not deployed to detected network.')
+    }
+  }
+
+  createPost(content) {
+    this.setState({ loading: true })
+    this.state.socialNetwork.methods.createPost(content).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  tipPost(id, tipAmount) {
+    this.setState({ loading: true })
+    this.state.socialNetwork.methods.tipPost(id).send({ from: this.state.account, value: tipAmount })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      socialNetwork: null,
+      postCount: 0,
+      posts: [],
+      loading: true
+    }
+
+    this.createPost = this.createPost.bind(this)
+    this.tipPost = this.tipPost.bind(this)
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
-        </div>
+        <Navbar account={this.state.account} />
+        { this.state.loading
+          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+          : <Main
+              posts={this.state.posts}
+              createPost={this.createPost}
+              tipPost={this.tipPost}
+            />
+        }
       </div>
     );
   }
---
+++ src/components/Main.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import Identicon from 'identicon.js';
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
+            <div className="content mr-auto ml-auto">
+              <p>&nbsp;</p>
+                <form onSubmit={(event) => {
+                  event.preventDefault()
+                  const content = this.postContent.value
+                  this.props.createPost(content)
+                }}>
+                <div className="form-group mr-sm-2">
+                  <input
+                    id="postContent"
+                    type="text"
+                    ref={(input) => { this.postContent = input }}
+                    className="form-control"
+                    placeholder="What's on your mind?"
+                    required />
+                </div>
+                <button type="submit" className="btn btn-primary btn-block">Share</button>
+              </form>
+              <p>&nbsp;</p>
+              { this.props.posts.map((post, key) => {
+                return(
+                  <div className="card mb-4" key={key} >
+                    <div className="card-header">
+                      <img
+                        className='mr-2'
+                        width='30'
+                        height='30'
+                        src={`data:image/png;base64,${new Identicon(post.author, 30).toString()}`}
+                      />
+                      <small className="text-muted">{post.author}</small>
+                    </div>
+                    <ul id="postList" className="list-group list-group-flush">
+                      <li className="list-group-item">
+                        <p>{post.content}</p>
+                      </li>
+                      <li key={key} className="list-group-item py-2">
+                        <small className="float-left mt-1 text-muted">
+                          TIPS: {window.web3.utils.fromWei(post.tipAmount.toString(), 'Ether')} ETH
+                        </small>
+                        <button
+                          className="btn btn-link btn-sm float-right pt-0"
+                          name={post.id}
+                          onClick={(event) => {
+                            let tipAmount = window.web3.utils.toWei('0.1', 'Ether')
+                            console.log(event.target.name, tipAmount)
+                            this.props.tipPost(event.target.name, tipAmount)
+                          }}
+                        >
+                          TIP 0.1 ETH
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                )
+              })}
+            </div>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
